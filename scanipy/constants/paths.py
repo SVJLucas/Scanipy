970b4da8e047ff9a6e01b072540752c0c554d4be
SVJLucas <lucas.jose.veloso.de.souza@gmail.com>
2023-09-11 07:27:10 +0200

    Updating tablesdinfer model definition and weights

--- scanipy/constants/paths.py
+++ scanipy/constants/paths.py
@@ -3,3 +3,4 @@ import os
 # Model's path
 PATH_TO_IMAGE_TO_LATEX_MODEL = os.path.join('deeplearning', 'weights', 'pix2tex-LaTeX-OCR-v-09-set-2023.pkl')
 PATH_TO_TABLE_STRUCTURE_RECOGNITION_MODEL = os.path.join('deeplearning', 'weights', 'microsoft-table-transformer-structure-recognition-11-set-2023.pkl')
+PATH_TO_TABLE_FINDER_MODEL = os.path.join('deeplearning', 'weights', 'microsoft-table-transformer-detection-11-set-2023.pkl')
---
+++ scanipy/deeplearning/models/tablefinder.py
@@ -0,0 +1,49 @@
+'''
+This file contains the implementation of TableStructureAnalyzer class
+
+Created by:
+    Lucas José (lucas.jose.veloso.de.souza@gmail.com)
+    Date: September 11, 2023
+'''
+
+import pickle
+from transformers import pipeline
+from constats.paths import PATH_TO_TABLE_FINDER_MODEL
+
+
+class TableFinder:
+    """
+    Locate tables within images using a fine-tuned Table Transformer model for table detection.
+
+    This model is a Table Transformer (DETR) variant trained by Microsoft on the PubTables1M dataset.
+    It is designed to detect tables in images.
+
+    Source: https://github.com/microsoft/table-transformer
+
+    Attributes:
+        model (object): The loaded fine-tuned Table Transformer model for table detection.
+
+    Example:
+        >>> finder = TableFinder()
+        >>> detection_result = finder(image)
+    """
+
+    def __init__(self):
+        """
+        Initialize the TableFinder class by loading the pre-trained model for table detection.
+        """
+        # Load the pre-trained Table Transformer model from a pickle file for table detection
+        self.model = pickle.load(open(PATH_TO_TABLE_FINDER_MODEL, 'rb'))
+
+    def __call__(self, image):
+        """
+        Detect tables in a given image using the pre-trained Table Transformer model.
+
+        Args:
+            image: The image in which tables are to be detected.
+
+        Returns:
+            object: The detected table information including bounding boxes and other attributes.
+        """
+        # Use the loaded model to detect tables in the given image
+        return self.model(image)
