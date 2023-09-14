3ef436873b2bfc9e3156a7f7863429448450ff84
Lucas José Velôso de Souza <60625769+SVJLucas@users.noreply.github.com>
2023-09-14 14:00:15 +0200

    Update __init__.py

--- scanipy/elements/__init__.py
+++ scanipy/elements/__init__.py
@@ -1,4 +1,7 @@
 from .equation import EquationElement
 from .image import ImageElement
 from .table import TableElement
+from .title import TitleElement
 from .text import TextElement
+from .element import Element
+
