73867643f11d18fa32b402f328988fddd12e25fb
guimlherme <guilhermeoliveira5001@gmail.com>
2023-09-14 13:02:38 +0200

    Fixed broken scripts

--- scanipy/extractors/text.py
+++ scanipy/extractors/text.py
@@ -31,7 +31,7 @@ class TextExtractor(Extractor):
             self.doctr_ocr.cuda()
             device = "cuda:0"
 
-        self.model = lp.Detectron2LayoutModel('lp://PubLayNet/mask_rcnn_X_101_32x8d_FPN_3x/config',
+        self.model = lp.models.Detectron2LayoutModel('lp://PubLayNet/mask_rcnn_X_101_32x8d_FPN_3x/config',
                                               extra_config=["MODEL.ROI_HEADS.SCORE_THRESH_TEST", 0.8],
                                               label_map={0: "Text", 1: "Title", 2: "List", 3: "Table", 4: "Figure"},
                                               device=device)
