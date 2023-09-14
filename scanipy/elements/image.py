3fcb399f2bf59118500ae89d16a3cfb3e69569fa
Lucas José Velôso de Souza <60625769+SVJLucas@users.noreply.github.com>
2023-09-14 13:16:47 +0200

    Update image.py

--- scanipy/elements/image.py
+++ scanipy/elements/image.py
@@ -1,30 +1,137 @@
+from typing import Union, Any
 from .element import Element
 import os
 
+# Define the ImageElement class, which inherits from the Element class
 class ImageElement(Element):
     """
     Represents an image element within a document.
 
     Attributes:
-        key (str): A unique key for the image element.
-        content: The image content.
-        image_ext (str): The file extension for the image (e.g., 'jpg', 'png').
+        unique_key (str): A unique identifier for the image element.
+        image_content (Any): The actual content of the image.
+        image_extension (str): The file extension for the image (e.g., 'jpg', 'png').
     """
 
-    def __init__(self, x_min, y_min, x_max, y_max, pipeline_step, key, content, image_ext):
+    def __init__(self, x_min: int, y_min: int, x_max: int, y_max: int, pipeline_step: Union[int, None] = None):
+        """
+        Initialize an ImageElement object.
+
+        Args:
+            x_min (int): The minimum x-coordinate.
+            y_min (int): The minimum y-coordinate.
+            x_max (int): The maximum x-coordinate.
+            y_max (int): The maximum y-coordinate.
+            pipeline_step (Union[int, None], optional): The pipeline step, can be None.
+
+        Raises:
+            ValueError: If x_min >= x_max or y_min >= y_max.
+            TypeError: If the types of the arguments are not as expected.
+        """
+        # Verify the input variable types
+        if not all(isinstance(var, int) for var in [x_min, y_min, x_max, y_max]):
+            raise TypeError("Coordinates must be integers")
+        if pipeline_step is not None and not isinstance(pipeline_step, int):
+            raise TypeError("pipeline_step must be an integer or None")
+
+        # Initialize instance variables by calling the parent class constructor
         super().__init__(x_min, y_min, x_max, y_max, pipeline_step)
-        self.key = key
-        self.content = content
-        self.image_ext = image_ext
 
-    def print(self, output_folder):
-        """
-        Print the image element to the document and save the image to a file.
+        # Initialize additional instance variables specific to ImageElement
+        self.unique_key = None
+        self.image_content = None
+        self.image_extension = None
 
-        :param output_folder: The folder where the image file will be saved.
-        :return: Markdown representation of the image element.
+    @property
+    def unique_key(self) -> str:
         """
-        filename = f"{self.key}.{self.image_ext}"
-        output_path = os.path.join(output_folder, filename)
-        self.content.save(open(output_path, "wb"))
+        Gets the unique key of the image element.
+
+        Returns:
+            str: The unique key of the image element.
+        """
+        return self._unique_key
+
+    @unique_key.setter
+    def unique_key(self, value: str):
+        """
+        Sets the unique key of the image element.
+
+        Args:
+            value (str): The new unique key.
+
+        Raises:
+            TypeError: If the provided value is not a string.
+        """
+        if not isinstance(value, str):
+            raise TypeError("unique_key must be a string")
+        self._unique_key = value
+
+    @property
+    def image_content(self) -> Any:
+        """
+        Gets the content of the image element.
+
+        Returns:
+            Any: The content of the image element.
+        """
+        return self._image_content
+
+    @image_content.setter
+    def image_content(self, value: Any):
+        """
+        Sets the content of the image element.
+
+        Args:
+            value (Any): The new content for the image element.
+        """
+        # Type verification can be more specific depending on what 'Any' encompasses
+        self._image_content = value
+
+    @property
+    def image_extension(self) -> str:
+        """
+        Gets the file extension of the image element.
+
+        Returns:
+            str: The file extension of the image element.
+        """
+        return self._image_extension
+
+    @image_extension.setter
+    def image_extension(self, value: str):
+        """
+        Sets the file extension of the image element.
+
+        Args:
+            value (str): The new file extension for the image element.
+
+        Raises:
+            TypeError: If the provided value is not a string.
+        """
+        if not isinstance(value, str):
+            raise TypeError("image_extension must be a string")
+        self._image_extension = value
+
+    def generate_markdown(self, output_directory) -> str:
+        """
+        Generate the Markdown representation of the image element and save the image to a file.
+
+        Args:
+            output_directory (str): The directory where the image file will be saved.
+
+        Returns:
+            str: Markdown representation of the image element.
+        """
+        # Create the filename for the image
+        filename = f"{self.unique_key}.{self.image_extension}"
+
+        # Create the full path where the image will be saved
+        output_path = os.path.join(output_directory, filename)
+
+        # Save the image content to the file
+        self.image_content.save(open(output_path, "wb"))
+
+        # Return the Markdown representation of the image
         return f"\n![image]({filename})\n\n"
+
